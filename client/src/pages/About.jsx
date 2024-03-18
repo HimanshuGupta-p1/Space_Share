ffb6d9b40224f736b3282cedda9fbe21a019ecd2
Himanshu <ghimanshugupta70@gmail.com>
2024-03-18 16:06:40 +0530

    fix: styles of about section

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from "../assets/logo.png"
+import aboutpic from '../assets/pngegg.png'
 import Avanish from "../assets/Avanish.png"
 import Kushagra from "../assets/Kushagra.png"
 import Himanshu from "../assets/Himanshu.png"
@@ -9,25 +9,34 @@ const About = () => {
   return (
     <><h1 className="text-white text-center font-serif text-4xl mt-6 mb-4">Welcome, To Space Share</h1>
     
-    <div className="h-50 w-50 blue-glassmorphism rounded-md bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10 border border-gray-100 mx-20  grid grid-cols-1 sm:grid-cols-2">
+    <div className="h-50 w-50 blue-glassmorphism rounded-md
+     bg-clip-padding backdrop-filter backdrop-blur-xl 
+    bg-opacity-10 border border-gray-100 md:mx-20 mx-2  grid grid-cols-1 lg:grid-cols-2
+    items-center">
 
         <div>
-        <h1 className='text-white text-left-top ml-5 text-4xl mt-6'>Our Vision</h1>
+        <h1 className='text-white text-center md:text-left-top ml-5 text-4xl mt-6 '>Our Vision</h1>
         <ul>
-        <p className='mt-4 ml-5 mb-3 text-left-top text-xl text-slate-200 mr-2 font-normal'>A disk space renting system using blockchain technology is a platform that allows users to rent out or lease unused storage space on their devices, such as computers, to other users who need it. The platform is built on a blockchain network, which provides secure and transparent transactions between the renters and the providers. The blockchain-based disk space renting system can solve the problem of unused storage space, which is a prevalent issue in today's digital world. Many users have unused storage space on their devices, while others need extra storage space for their data. The platform can connect these users and allow them to transact in a decentralized, secure, and efficient manner. The system works by creating a marketplace where users can list their available storage space and set their rental prices. Other users who need storage space can browse the listings and rent the space they need for a specified period. The rental transactions are recorded on the blockchain, providing transparency and security for all parties involved.</p>
+        <p className='mt-4 ml-5 mb-3 text-left-top text-xl text-justify
+        text-slate-200 mr-2 font-normal'>A disk space renting system using blockchain technology is a platform that allows users to rent out or lease unused storage space on their devices, such as computers, to other users who need it. 
+        The platform is built on a blockchain network, which provides secure and transparent transactions between the renters and the providers. 
+        The blockchain-based disk space renting system can solve the problem of unused storage space, which is a prevalent issue in today's digital world. 
+        Many users have unused storage space on their devices, while others need extra storage space for their data. The platform can connect these users and allow them to transact in a decentralized, secure, and efficient manner. 
+        The system works by creating a marketplace where users can list their available storage space and set their rental prices. Other users who need storage space can browse the listings and rent the space they need for a specified period. 
+        The rental transactions are recorded on the blockchain, providing transparency and security for all parties involved.</p>
         </ul>
         </div>
+        <img src = {aboutpic} alt = "aboutPic"/>
      
-      <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.dev/svgjs" viewBox="0 0 800 800"><g transform="matrix(1,0,0,1,0,0)"><circle r="203.5" cx="800" cy="506" stroke-width="6" stroke="#ff3895" fill="none" opacity="0.1" transform="matrix(-1.161264033815549,-0.8822239560150357,0.2047621808880289,-0.5512747116339981,999.071477606178,1038.2985748546844)"></circle><circle r="200.52777777777777" cx="777.7777777777778" cy="500.1111111111111" stroke-width="6.555555555555555" stroke="#f9419d" fill="none" opacity="0.15000000000000002" transform="matrix(-1.0912937561775355,-0.9370635402768887,0.385256955483854,-0.44139263032215514,887.9162761189073,996.8003509329819)"></circle><circle r="197.55555555555554" cx="755.5555555555555" cy="494.22222222222223" stroke-width="7.111111111111111" stroke="#f349a4" fill="none" opacity="0.2" transform="matrix(-0.9957679726138231,-0.9786256204971873,0.549441529240591,-0.3263887584423016,768.2190887947195,950.5425572498402)"></circle><circle r="194.58333333333334" cx="733.3333333333334" cy="488.3333333333333" stroke-width="7.666666666666667" stroke="#ed50ab" fill="none" opacity="0.25" transform="matrix(-0.8777831398769071,-1.0064701550525375,0.6940609186385497,-0.2078193703560034,643.9589947051745,900.1293629302543)"></circle><circle r="191.61111111111111" cx="711.1111111111111" cy="482.44444444444446" stroke-width="8.222222222222221" stroke="#e657b2" fill="none" opacity="0.30000000000000004" transform="matrix(-0.7408101214235361,-1.020334170990599,0.8163632190511083,-0.08727417542788457,518.9594342927817,846.1303954859446)"></circle><circle r="188.63888888888889" cx="688.8888888888889" cy="476.55555555555554" stroke-width="8.777777777777779" stroke="#e05db9" fill="none" opacity="0.35" transform="matrix(-0.588621761445024,-1.0201337885277293,0.914140637276916,0.03364327679646856,396.8157115672294,789.083993083107)"></circle><circle r="185.66666666666666" cx="666.6666666666667" cy="470.6666666666667" stroke-width="9.333333333333334" stroke="#d962bf" fill="none" opacity="0.4" transform="matrix(-0.4252156536392061,-1.0059641290842334,0.9857606864738043,0.1533352461398937,280.8294759604914,729.5025135482645)"></circle><circle r="182.69444444444446" cx="644.4444444444445" cy="464.77777777777777" stroke-width="9.88888888888889" stroke="#d268c5" fill="none" opacity="0.45000000000000007" transform="matrix(-0.25473348626107234,-0.9780971167574593,1.030186931196758,0.2702292818536154,173.95145638466857,667.8790141759619)"></circle><circle r="179.72222222222223" cx="622.2222222222222" cy="458.8888888888889" stroke-width="10.444444444444445" stroke="#cb6dcb" fill="none" opacity="0.5" transform="matrix(-0.08137841192935905,-0.9369772065564321,1.0469888739248552,0.38279731150266255,78.73339881158375,604.6951334040716)"></circle><circle r="176.75" cx="600" cy="453" stroke-width="11" stroke="#c472d1" fill="none" opacity="0.55" transform="matrix(0.09066807001248783,-0.8832150957375208,1.0363407824331619,0.48957412194703176,-2.709863936303236,540.4293962269676)"></circle><circle r="173.77777777777777" cx="577.7777777777778" cy="447.1111111111111" stroke-width="11.555555555555555" stroke="#bc77d6" fill="none" opacity="0.6" transform="matrix(0.2573282194294374,-0.8175794969396997,0.9990094665298659,0.5891750230115937,-68.7279468056712,475.56564019724993)"></circle><circle r="170.80555555555554" cx="555.5555555555555" cy="441.22222222222223" stroke-width="12.11111111111111" stroke="#b47bdc" fill="none" opacity="0.65" transform="matrix(0.414707667016161,-0.7409870732785733,0.9363312197645144,0.6803124940457249,-118.15148236829054,410.6008910975105)"></circle><circle r="167.83333333333331" cx="533.3333333333334" cy="435.3333333333333" stroke-width="12.666666666666668" stroke="#ab7fe1" fill="none" opacity="0.7000000000000001" transform="matrix(0.5591710929372296,-0.6544906558885435,0.8501783433705932,0.7618116253604672,-150.29776185865558,346.0523079561942)"></circle><circle r="164.86111111111111" cx="511.11111111111114" cy="429.44444444444446" stroke-width="13.222222222222221" stroke="#a284e6" fill="none" opacity="0.75" transform="matrix(0.687412604191674,-0.5592658833827715,0.7429158625970925,0.8326241803667607,-164.96551886569728,282.46280612535935)"></circle><circle r="161.88888888888889" cx="488.8888888888889" cy="423.55555555555554" stroke-width="13.777777777777779" stroke="#9988ec" fill="none" opacity="0.8" transform="matrix(0.7965195378276339,-0.45659642012600854,0.6173492264640307,0.891841119971105,-162.4192117964186,220.40482869339198)"></circle><circle r="158.91666666666666" cx="466.6666666666667" cy="417.66666666666663" stroke-width="14.333333333333334" stroke="#8f8cf1" fill="none" opacity="0.85" transform="matrix(0.884028535783063,-0.34785792589789055,0.4766639477672154,0.9387034482427349,-143.36338487963224,160.48206966926261)"></circle><circle r="155.94444444444446" cx="444.44444444444446" cy="411.77777777777777" stroke-width="14.88888888888889" stroke="#8490f5" fill="none" opacity="0.9" transform="matrix(0.947972876796241,-0.23450096329476625,0.3243582880116686,0.9726112573582937,-108.90797799194718,103.32885177824699)"></circle><circle r="152.97222222222223" cx="422.22222222222223" cy="405.8888888888889" stroke-width="15.444444444444445" stroke="#7893fa" fill="none" opacity="0.9500000000000001" transform="matrix(0.9869202089407757,-0.1180330409280306,0.16417021931687234,0.9931308701489957,-60.5252997347547,49.606977081203695)"></circle><circle r="150" cx="400" cy="400" stroke-width="16" stroke="#6b97ff" fill="none" opacity="1" transform="matrix(1,0,0,1,0,0)"></circle></g></svg>        
-
+      
     </div>
     <><h1 className="text-white text-center font-serif text-4xl mt-6 mb-4">Development Team</h1>
     <p className='text-slate-200 text-center font-serif text-xl'>"Empowering Innovation, Building Tomorrow."</p>
     </>
-    <div className='h-50 w-50 pink-glassmorphism rounded-md bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10 border border-gray-100 mx-20 grid grid-cols-1 sm:grid-cols-4 mt-2'>
+    <div className='h-50 w-50 pink-glassmorphism rounded-md bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10 md:mx-20 grid grid-cols-1 lg:grid-cols-4 md:mt-2'>
     <div className="shadow-xl mx-3 my-3 rounded-md blue-glassmorphism">
   <figure><img src={Himanshu} alt="Shoes" /></figure>
-  <div className="card-body bg-gray-200 text-black">
+  <div className="card-body bg-transparent items-center text-white">
     <h2 className="card-title">Himanshu Gupta</h2>
     <p>Team Leader</p>
     <div>
@@ -39,7 +48,7 @@ const About = () => {
 </div>
 <div className="blue-glassmorphism shadow-xl mx-3 my-3 rounded-md">
   <figure><img src={Avanish} alt="Avanish" /></figure>
-  <div className="card-body bg-gray-200 text-black">
+  <div className="card-body bg-transparent items-center text-white">
     <h2 className="card-title">Avanish Srivastava</h2>
     <p>Team Member</p>
     <div>
@@ -51,7 +60,7 @@ const About = () => {
 </div>
 <div className="blue-glassmorphism shadow-xl mx-3 my-3 rounded-md">
   <figure><img src={Prakhar} alt="Shoes" /></figure>
-  <div className="card-body bg-gray-200 text-black">
+  <div className="card-body bg-transparent items-center text-white">
     <h2 className="card-title">Prakhar Pandey</h2>
     <p>Team Member</p>
     <div>
@@ -63,7 +72,7 @@ const About = () => {
 </div>
 <div className="blue-glassmorphism shadow-xl mx-3 my-3 rounded-md">
   <figure><img src={Kushagra} alt="Shoes" /></figure>
-  <div className="card-body bg-gray-200 text-black">
+  <div className="card-body bg-transparent items-center text-white">
     <h2 className="card-title">Kushagra Upadhyay</h2>
     <p>Team Member</p>
     <div>
